c9b7a44d706db4da8dacca2233e83f5964dca56d
Nico Schottelius <nico@bento.schottelius.org>
2014-03-12 23:44:39 +0100

    eine pruefung an einem zeitpunkt
    Signed-off-by: Nico Schottelius <nico@bento.schottelius.org>

--- pruefung.sh
+++ pruefung.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 # Skript um die Arbeit zu bewerten
 
+if [ "$#" -ne 1 ]; then
+    echo $0 studi >&2
+    exit 1
+fi
+
+studi=$1; shift
+
+cd "repos/$studi"
+
 # Abbrechen, falls ein Teil nicht bestanden ist
 set -e
 
